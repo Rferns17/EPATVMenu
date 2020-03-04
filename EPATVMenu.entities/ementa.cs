e816f61a53df126c967b53a79b9283be735ea08a
elcubano05 <nantobino2@gmail.com>
2020-03-04 15:56:28 +0000

    20200304-Bruno-2

---
+++ EPATVMenu.entities/ementa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace epatvmenu.ementa
+{
+    public class Ementa
+    {
+        private int id;
+        private DateTime data;
+        private string diaSemana;
+        private int idUtilizador;
+
+        public Ementa() { }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public string DiaSemana
+        {
+            get { return diaSemana; }
+            set { diaSemana = value; }
+        }
+
+        public int IdUtilizador
+        {
+            get { return idUtilizador; }
+            set { idUtilizador = value; }
+        }
+    }
+}
